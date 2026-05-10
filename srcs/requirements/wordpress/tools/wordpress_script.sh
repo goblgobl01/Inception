1f52852e392ddd81535b78b6ff6abc06a6529116
mmaarafi <maarafimohammed@gmail.com>
2026-05-10 09:36:07 +0100

    alo

--- srcs/requirements/wordpress/tools/wordpress_script.sh
+++ srcs/requirements/wordpress/tools/wordpress_script.sh
@@ -8,18 +8,20 @@ while ! mysqladmin ping -hmariadb -u$MYSQL_USER -p$MYSQL_PASSWORD --silent;
 do
 	sleep 1
 done
-if [ ! -f /var/www/wordpress/index.php ]
+if [ ! -f /var/www/wordpress/wp-config.php ]
 then
 	wp core download --path=/var/www/wordpress --allow-root
-	wp config create --allow-root --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=mariadb
-	wp core install --url=https://${DOMAIN_NAME} \
-					--title="Inception" \
-					--admin_user=${WP_ADMIN_USER} \
-					--admin_password=${WP_ADMIN_PASSWORD} \
-					--admin_email=${WP_ADMIN_EMAIL} \
-					--skip-email \
-					--allow-root
-	 wp user create ${WP_USER} ${WP_USER_EMAIL} \
+	wp config create --allow-root --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=mariadb 
+	wp core install --path=/var/www/wordpress \
+				--url=https://${DOMAIN_NAME} \
+				--title="Inception" \
+				--admin_user=${WP_ADMIN_USER} \
+				--admin_password=${WP_ADMIN_PASSWORD} \
+				--admin_email=${WP_ADMIN_EMAIL} \
+				--skip-email \
+				--allow-root
+
+	wp user create ${WP_USER} ${WP_USER_EMAIL} \
 					--role=author \
 					--user_pass=${WP_USER_PASSWORD} \
 					--path=/var/www/wordpress \
